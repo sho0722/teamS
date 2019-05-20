bf512db1ef73531206e4028c35ef4e8ba3784116
nasciiy <secondlife.kz@gmail.com>
2019-05-20 17:07:36 +0900

    仕上がりに近いよ

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+$(function(){
+
+// 矢印がクリックされたら
+
+$(".arrow").click(function(){
+    // 画面のトップまでスクロールする
+    $("html").animate({scrollTop:0} , 1000);
+
+});
+
+
+// 画面がスクロールされたら
+$(window).scroll(function(){
+       
+    // 矢印の表示・非表示を指定
+    let scrollTop = $(document).scrollTop();
+
+    // 矢印の表示・非表示を指定
+    if (scrollTop > 300){
+        // 500pxを超えたらフェイドイン
+        $(".arrow").fadeIn();
+    } else {
+        // それ以外はフェイドアウト
+        $(".arrow").fadeOut();
+        
+    };
+
+
+
+});
+
+});
