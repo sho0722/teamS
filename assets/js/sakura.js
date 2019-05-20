c638273d5c97f5724f3b24c752ca4845c26f5a94
sakutoro <s_kinjyo1904@pam.co.jp>
2019-05-20 14:38:45 +0900

    追加分

--- assets/js/sakura.js
+++ assets/js/sakura.js
@@ -1,18 +1,38 @@
 $(function() {
-    // どこまでがクリックされたら
-    $(".where").click(function() {
-        let s = $(this).siblings().slideDown();
-        console.log(s); 
+    // どこからがクリックフォーカスされたら
+    $(".where").focus(function() {
+        let s = $(this).next().slideDown();
+
+        console.log('where'); 
         
     });
-    $("html").click(function() {
-        $("label ul").slideUp();
+
+    //  
+    $(".dbox li").click(function() {
+        // テキスト取ってきて
+        let text = $(this).text();
+        console.log(text);
+        // 入力欄に選択されたテキスト追加
+        $(".where").val(text);
+        // .dbox liのulをスライドアップ
+        $(this).parent().slideUp();
+
+       
     })
-    
+
     // どこまでがクリックされたら
-    $(".Where-until").click(function() {
-       let n =  $(this).siblings().slideDown();
-       console.log(n); 
+    $(".Where-until").focus(function() {
+       let n =  $(this).next().slideDown();
+       console.log('Where-until'); 
+    })
+
+    $(".wbox li").click(function() {
+        let text = $(this).text();
+        console.log(text);
+        $(".Where-until").val(text);
+        $(this).parent().slideUp();
+
+       
     })
 
 })
