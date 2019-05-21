fdc0fdee44246dc6551de270e58d54bd31b1000d
Sho Tanaka <swallowtail.722.kyoto@gmail.com>
2019-05-21 12:11:43 +0900

    jQueryで動きつけました

--- assets/js/sho.js
+++ assets/js/sho.js
@@ -3,7 +3,54 @@ $(function() {
     $(window).scroll(function() {
         let scrollTop = $(window).scrollTop();
 
-        console.log(scrollTop);
+        if (scrollTop > 40) {
+            $(".date").fadeIn("slow");
+        }
+
+        if (scrollTop > 100) {
+            $(".time").fadeIn("slow");
+        }
+
+        if (scrollTop > 200) {
+            $(".people").fadeIn("slow");
+        }
+
+        if (scrollTop > 400) {
+            $(".car").fadeIn("slow");
+        }
+
+        if (scrollTop > 600) {
+            $(".like").fadeIn("slow");
+        }
+
+        if (scrollTop > 800) {
+            $(".purpose").fadeIn("slow");
+        }
+
+        let driverATop = $(".driverA").offset().top;
+        if (scrollTop > driverATop - 400) {
+            $(".driverA").animate({right: 0}, 1000);
+        }
+
+        let driverBTop = $(".driverB").offset().top;
+        if (scrollTop > driverBTop - 400) {
+            $(".driverB").animate({right: 0}, 1000);
+        }
+
+        let driverCTop = $(".driverC").offset().top;
+        if (scrollTop > driverCTop - 400) {
+            $(".driverC").animate({right: 0}, 1000);
+        }
+
+        let driverDTop = $(".driverD").offset().top;
+        if (scrollTop > driverDTop - 400) {
+            $(".driverD").animate({right: 0}, 1000);
+        }
+
+        let driverETop = $(".driverE").offset().top;
+        if (scrollTop > driverETop - 400) {
+            $(".driverE").animate({right: 0}, 1000);
+        }
     })
     
 })
