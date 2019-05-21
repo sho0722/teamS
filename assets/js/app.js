723d71568073fb0b0e189f9343fe0a4ebe39f6a3
Sho Tanaka <swallowtail.722.kyoto@gmail.com>
2019-05-21 13:41:21 +0900

    hoge

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,33 @@
 $(function(){
 
-// 矢印がクリックされたら
+    // 矢印がクリックされたら
+    
+    $(".arrow").click(function(){
+        // 画面のトップまでスクロールする
+        $("html").animate({scrollTop:0} , 1000);
+    
+    });
+    
+    
+    // 画面がスクロールされたら
+    $(window).scroll(function(){
+        // 矢印の表示・非表示を指定
+        let scrollTop = $(document).scrollTop();
+    
+        // 矢印の表示・非表示を指定
+        if (scrollTop > 300){
+            // 500pxを超えたらフェイドイン
+            $(".arrow").fadeIn();
+        } else {
+            // それ以外はフェイドアウト
+            $(".arrow").fadeOut();
+        }
 
-$(".arrow").click(function(){
-    // 画面のトップまでスクロールする
-    $("html").animate({scrollTop:0} , 1000);
-
-});
-
-
-// 画面がスクロールされたら
-$(window).scroll(function(){
-       
-    // 矢印の表示・非表示を指定
-    let scrollTop = $(document).scrollTop();
-
-    // 矢印の表示・非表示を指定
-    if (scrollTop > 300){
-        // 500pxを超えたらフェイドイン
-        $(".arrow").fadeIn();
-    } else {
-        // それ以外はフェイドアウト
-        $(".arrow").fadeOut();
-        
-    };
-
-
-
-});
-
+        if ( scrollTop > 50 ){
+            $(".takeme").fadeIn();
+        } else {
+            $(".takeme").fadeOut();
+        }
+    });
+    
 });
